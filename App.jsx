95939c38a02dc6863f13c22f8f630d7f2e72e327
MelaniSandalika <79406168+MelaniSandalika@users.noreply.github.com>
2021-06-06 13:13:35 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Header from "./Footer";
+import Header from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Mr.Shaurya Sinhha.We covered
+        everything from scratch including Javascript, HTML and React.js. Thank
+        you so much for this amazing Bootcamp. We need more Bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
